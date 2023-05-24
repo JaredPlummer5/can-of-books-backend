18bdea73b46b2db941fa7a7cf6d0a61af5c74e4d
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-05-24 11:17:29 -0500

    Delet function completed

--- server.js
+++ server.js
@@ -53,24 +53,61 @@ app.get('/books', async (request, response) => {
         console.log(error)
     }
 }).post('/books', async function (req, res) {
+    // Define the route '/books' for POST requests. 
+    //This function is asynchronous, meaning it can work in the background, allowing your server to handle other requests concurrently.
     try {
+        // From the request body, get the title, description, and status of the book to be created.
         let title = req.body.title
         let description = req.body.description
         let status = req.body.status
+
+        // Connect to the database. The DATABASE_URL environment variable holds the connection string.
+        // 'await' is used here because this operation is asynchronous and we need to ensure the connection is established before proceeding.
         await mongoose.connect(process.env.DATABASE_URL)
 
+        // Create a new book document in the database. The 'await' keyword is used because 'Book.create()' is an asynchronous function.
         let newBook = await Book.create({
             title: title,
             description: description,
             status: status
         });
+
+        // After creating the new book, send it as the response to the client. 
+        //This could be used to confirm that the book was created successfully.
         res.send(newBook)
+
+        // Disconnect from the database to free up resources.
         mongoose.disconnect();
 
     } catch (error) {
+        // If any error occurred above (database connection, book creation, etc.), send a 500 (Internal Server Error) status code along with the error message to the client.
         res.status(500).send(error.message);
     }
-})
+}).delete('/books/:id', async (req, res) => {
+    try {
+        const id = req.params.id; // Get the id from the request parameters
+
+        // Connect to the MongoDB database
+        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // Delete the book with the given id
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            res.status(404).send('No book found with the given id');
+            return;
+        }
+
+        res.send('Book deleted successfully');
+    } catch (error) {
+        // If an error occurred, send back a 500 status and the error message
+        res.status(500).send(error.message);
+    } finally {
+        // Always disconnect from the database when you're done
+        mongoose.disconnect();
+    }
+
+});
 
 // Start our server listening on the specified PORT.
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
