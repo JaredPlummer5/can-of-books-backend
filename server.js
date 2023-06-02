ae44df93039278a127dd6e6ff60364fefddc5e6f
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-06-01 21:46:22 -0500

    Added an email field in the book schema so that a user can make book for their self

--- server.js
+++ server.js
@@ -5,77 +5,130 @@ const Book = require('./models/Books');
 const mongoose = require('mongoose');
 const Seed = require("./seed");
 const app = express();
-const { auth } = require("express-oauth2-jwt-bearer");
+const axios = require('axios')
+const { expressjwt: jwt } = require('express-jwt')
+const jwks = require('jwks-rsa');
+
 app.use(cors());
 app.use(express.json());
 
-
-const checkJwt = auth({
-    audience: "https://dev-swwedz5mied7hhq5.us.auth0.com/api/v2/",
-    issuerBaseURL: `https://${process.env.DOMAIN_URL}/`,
-    algorithms: ["RS256"]
-});
-
 const PORT = process.env.PORT || 3001;
-//mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-app.get('/test', checkJwt, (request, response) => {
-    console.log(request)
-    response.send('test request received');
-});
 
-app.get('/books', checkJwt, async (request, response) => {
+const verifyJWT = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-swwedz5mied7hhq5.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'http://localhost:3001',
+    issuer: 'https://dev-swwedz5mied7hhq5.us.auth0.com/',
+    algorithms: ['RS256']
+}).unless({ path: ['/books'] });
+
+app.use(verifyJWT);
+
+app.get('/test', async (req, res) => {
+    console.log(req.auth);
+    const accessToken = req.headers.authorization.split(' ')[1];
     try {
-        await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        const user = await axios.get('https://dev-swwedz5mied7hhq5.us.auth0.com/userinfo', {
+            headers: {
+                authorization: `Bearer ${accessToken}`
+            }
         });
-        const books = await Book.find();
-        console.log(request)
-        //console.log(request.headers.authorization.user)
-        mongoose.disconnect();
-        response.json(books);
+        const userinfo = user.data;
+        console.log(userinfo);
+        res.send(userinfo);
     } catch (error) {
         console.log(error);
-        response.status(500).send('Internal Server Error');
+        res.status(500).send('Internal Server Error');
     }
-}).post('/books',checkJwt, async (req, res) => {
+});
+
+app.get('/books', async (req, res) => {
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log(request)
-        let title = req.body.title;
-        let description = req.body.description;
-        let status = req.body.status;
+        const accessToken = req.headers.authorization.split(' ')[1];
+        const user = await axios.get('https://dev-swwedz5mied7hhq5.us.auth0.com/userinfo', {
+            headers: {
+                authorization: `Bearer ${accessToken}`
+            }
+        });
+        const userinfo = user.data;
+        console.log(userinfo);
+        // Get the user from the JWT
+        const books = await Book.find({ userEmail: userinfo.email }); // Query books based on user email
 
-        let newBook = await Book.create({
+        mongoose.disconnect();
+        res.json(books);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/books', async (req, res) => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+
+        console.log(req.headers.authorization.split(' ')[1])
+        const { title, description, status } = req.body;
+        const accessToken = req.headers.authorization.split(' ')[1];
+        const user = await axios.get('https://dev-swwedz5mied7hhq5.us.auth0.com/userinfo', {
+            headers: {
+                authorization: `Bearer ${accessToken}`
+            }
+        });
+        const userinfo = user.data;
+        console.log(userinfo);
+        const newBook = await Book.create({
             title: title,
             description: description,
-            status: status
+            status: status,
+            userEmail: userinfo.email // Save the user's email with the book
         });
+console.log(newBook)
         res.send(newBook);
         mongoose.disconnect();
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server Error');
     }
-}).delete('/books/:id', checkJwt, async (req, res) => {
+});
+
+app.delete('/books/:id', async (req, res) => {
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        const id = req.params.id;
 
-        const result = await Book.findByIdAndDelete(id);
+        const id = req.params.id;
+        const accessToken = req.headers.authorization.split(' ')[1];
+        const user = await axios.get('https://dev-swwedz5mied7hhq5.us.auth0.com/userinfo', {
+            headers: {
+                authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        const userinfo = user.data;
+        console.log(userinfo);
+        const result = await Book.findOneAndDelete({ _id: id, userEmail: userinfo.email }); // Query book based on id and user email
+
         if (!result) {
             res.status(404).send('No book found with the given id');
             return;
         }
-        const booksLeft = await Book.find()
-    
 
+        const booksLeft = await Book.find({ userEmail: userinfo.email }); // Fetch the remaining books based on user email
         res.send(booksLeft);
     } catch (error) {
         console.log(error);
@@ -83,27 +136,38 @@ app.get('/books', checkJwt, async (request, response) => {
     } finally {
         mongoose.disconnect();
     }
-}).put('/books/:id',checkJwt, async (req, res) => {
+});
+
+app.put('/books/:id', async (req, res) => {
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
+
         const id = req.params.id;
         const { title, description, status } = req.body;
-        
+        const accessToken = req.headers.authorization.split(' ')[1];
+        const user = await axios.get('https://dev-swwedz5mied7hhq5.us.auth0.com/userinfo', {
+            headers: {
+                authorization: `Bearer ${accessToken}`
+            }
+        });
 
-        const updatedBook = await Book.findByIdAndUpdate(id, {
-            title: title,
-            description: description,
-            status: status
-        }, { new: true });
+        const userinfo = user.data;
+        console.log(userinfo);
+        const updatedBook = await Book.findOneAndUpdate(
+            { _id: id, userEmail: userinfo.email }, // Query book based on id and user email
+            { title: title, description: description, status: status },
+            { new: true }
+        );
 
         if (!updatedBook) {
             res.status(404).send('No book found with the given id');
             return;
         }
-        const booksWithUpdate = await Book.find()
+
+        const booksWithUpdate = await Book.find({ userEmail: userinfo.email }); // Fetch the updated books based on user email
         res.send(booksWithUpdate);
     } catch (error) {
         console.log(error);
@@ -111,6 +175,18 @@ app.get('/books', checkJwt, async (request, response) => {
     } finally {
         mongoose.disconnect();
     }
-})
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+});
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    const status = error.status || 500;
+    const message = error.message || 'Internal server error';
+    res.status(status).send(message);
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
